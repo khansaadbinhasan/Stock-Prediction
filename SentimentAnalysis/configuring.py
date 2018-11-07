aa118be0fa345e53063bfeb5ea68da11df33527e
khansaadbinhasan <khansaadbinhasan@gmail.com>
2018-11-07 23:59:59 +0530

    mode old code to .gitignore

--- SentimentAnalysis/configuring.py
+++ SentimentAnalysis/configuring.py
@@ -1,11 +1,11 @@
 # DJIA files
-inputFileDJIA = '../../../Datasets/Datasets/DJIA/{}.csv'
-outputFileDJIA = '../../../Datasets/Datasets/DJIA/new{}.csv'
+inputFileDJIA = '../Datasets/DJIA/{}.csv'
+outputFileDJIA = '../Datasets/DJIA/new{}.csv'
 
 # Twitter files
-inputFileTwitter = '../../../Datasets/Datasets/Companies 2017/{}.csv'
-intermediateFileTwitter = '../../../Datasets/Datasets/New Companies/old{}.csv'
-outputFileTwitter = '../../../Datasets/Datasets/New Companies/{}.csv'
+inputFileTwitter = '../Datasets/Companies 2017/{}.csv'
+intermediateFileTwitter = '../Datasets/New Companies/old{}.csv'
+outputFileTwitter = '../Datasets/New Companies/{}.csv'
 
 # Do Preprocessing
 doTwitterPreprocessing = False
